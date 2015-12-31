ac2699d93d0885f5ce73d4cb9fe3f918fd7585d1
Suraj Kumar Jana <surajjana2@gmail.com>
2015-12-31 13:49:23 +0530

    new commit

--- test.py
+++ test.py
@@ -43,4 +43,4 @@ def cin(cin):
 	print "Avg. Capital : ",ac[0]
 
 	fo.close()
-	return "{\"cin_status\":{\"Company Name\":\""+cmp_name[0]+"\"}}"
+	return "{\"cin_status\":{\"Company Name\":\""+cmp_name[0]+"\",\"Registration No.\":\""+reg_no[0]+"\" "}}"
